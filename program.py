1782fee9f574eb0551133d4ea07a732bd3a22340
Deana-Mendoza <djmendoza@willamette.edu>
2025-01-31 11:11:05 -0800

    changed new file for bagjack

--- program.py
+++ program.py
@@ -1,8 +1,18 @@
 
 
-def main():
+from datastructures.bag import Bag
+
+#testing stuff for bag during class lecture 1/27/25
+def main(): 
     
-    print("Hello, World!")
+    bag=Bag()
+    bag.add("apple")
+    bag.add("pear")
+    bag.add("banana")
+
+    items=bag.distinct_items()
+
+    print(f"Distinct Items:{items}")
 
 
 
